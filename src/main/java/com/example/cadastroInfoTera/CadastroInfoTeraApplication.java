b3fe2e2df282690ca2c903051294c8a9c9cb06ba
Vinicius Ciscoto <viwdni@gmail.com>
2024-12-26 10:56:10 -0300

    Initial commit

---
+++ src/main/java/com/example/cadastroInfoTera/CadastroInfoTeraApplication.java
@@ -0,0 +1,13 @@
+package com.example.cadastroInfoTera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroInfoTeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroInfoTeraApplication.class, args);
+	}
+
+}
